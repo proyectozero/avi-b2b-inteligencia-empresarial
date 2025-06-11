c8fa69cbc205bf84277f9718948edaac9b0a5c61
proyectozero <proyectozeroavii@gmail.com>
2025-06-11 17:34:13 -0600

    Add scripts/openai/conn.php 1749684853303

---
+++ scripts/openai/conn.php
@@ -0,0 +1,9 @@
+<?php
+// conn.php
+require_once 'config.php';
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
